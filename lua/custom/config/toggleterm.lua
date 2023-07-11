a1d05857073d1ac3c81c806fdb0fe4f7d56f4361
Tri Pham <techcaotri@gmail.com>
2023-07-11 18:06:05 +0700

    Remap 'Toggle Terminal' related shortcuts

--- lua/custom/config/toggleterm.lua
+++ lua/custom/config/toggleterm.lua
@@ -1,2 +1,8 @@
 lvim.builtin.terminal.open_mapping=nil
+lvim.builtin.terminal.execs = {
+      { nil, "<M-h>", "Horizontal Terminal", "horizontal", 0.3 },
+      { nil, "<M-v>", "Vertical Terminal", "vertical", 0.4 },
+      { nil, "<M-i>", "Float Terminal", "float", nil },
+    }
+
 
