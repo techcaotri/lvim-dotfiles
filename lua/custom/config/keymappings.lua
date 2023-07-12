a5b870b7076ce6cb0fca67bea5fa81d3bc3de070
Tri Pham <techcaotri@gmail.com>
2023-07-12 13:45:57 +0700

    Reformat the files; prevent 'v' to display which-key help

--- lua/custom/config/keymappings.lua
+++ lua/custom/config/keymappings.lua
@@ -1,95 +1,99 @@
 local opts = {
-      mode = "n", -- NORMAL mode
-      buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-      silent = true, -- use `silent` when creating keymaps
-      noremap = true, -- use `noremap` when creating keymaps
-      nowait = true, -- use `nowait` when creating keymaps
-    }
+  mode = "n",         -- NORMAL mode
+  buffer = nil,       -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true,      -- use `silent` when creating keymaps
+  noremap = true,     -- use `noremap` when creating keymaps
+  nowait = true,      -- use `nowait` when creating keymaps
+}
 local mappings = {
-    -- Better window movement
-    ["<C-h>"] = { "<C-w>h", "Window move Left" },
-    ["<C-j>"] = { "<C-w>j", "Window move Down" },
-    ["<C-k>"] = { "<C-w>k", "Window move Up" },
-    ["<C-l>"] = { "<C-w>l", "Window move Right" },
+  -- Better window movement
+  ["<C-h>"] = { "<C-w>h", "Window move Left" },
+  ["<C-j>"] = { "<C-w>j", "Window move Down" },
+  ["<C-k>"] = { "<C-w>k", "Window move Up" },
+  ["<C-l>"] = { "<C-w>l", "Window move Right" },
 
-    -- Resize with arrows
-    ["<C-Up>"] = { ":resize -2<CR>", "Window resize Horizontal Decrease " },
-    ["<C-Down>"] = { ":resize +2<CR>", "Window resize Horizontal Increase" },
-    ["<C-Left>"] = { ":vertical resize -2<CR>", "Window resize Vertical Decrease" },
-    ["<C-Right>"] = { ":vertical resize +2<CR>", "Window resize Vertical Increase" } ,
+  -- Resize with arrows
+  ["<C-Up>"] = { ":resize -2<CR>", "Window resize Horizontal Decrease " },
+  ["<C-Down>"] = { ":resize +2<CR>", "Window resize Horizontal Increase" },
+  ["<C-Left>"] = { ":vertical resize -2<CR>", "Window resize Vertical Decrease" },
+  ["<C-Right>"] = { ":vertical resize +2<CR>", "Window resize Vertical Increase" },
 
-    -- Move current line / block with Alt-j/k a la vscode.
-    ["<A-j>"] = { ":m .+1<CR>==", "Move current line/block Up" },
-    ["<A-k>"] = { ":m .-2<CR>==", "Move current line/block Down" },
+  -- Move current line / block with Alt-j/k a la vscode.
+  ["<A-j>"] = { ":m .+1<CR>==", "Move current line/block Up" },
+  ["<A-k>"] = { ":m .-2<CR>==", "Move current line/block Down" },
 
-    -- QuickFix
-    ["]q"] = { ":cnext<CR>", "QuickFix Next" },
-    ["[q"] = { ":cprev<CR>", "QuickFix Prev" },
-    ["<C-q>"] = { ":call QuickFixToggle()<CR>", "QuickFix Toggle" },
+  -- QuickFix
+  ["]q"] = { ":cnext<CR>", "QuickFix Next" },
+  ["[q"] = { ":cprev<CR>", "QuickFix Prev" },
+  ["<C-q>"] = { ":call QuickFixToggle()<CR>", "QuickFix Toggle" },
 
-    -- save
-    ["<C-s>"] = { "<cmd> w <CR>", "Save file" },
+  -- save
+  ["<C-s>"] = { "<cmd> w <CR>", "Save file" },
 
-    -- Copy all
-    ["<C-c>"] = { "<cmd> %y+ <CR>", "Copy whole file" },
+  -- Copy all
+  ["<C-c>"] = { "<cmd> %y+ <CR>", "Copy whole file" },
 
-    -- cycle through buffers
-    ["<tab>"] = { "<cmd> BufferLineCycleNext <CR>", "Goto next buffer" },
+  -- cycle through buffers
+  ["<tab>"] = { "<cmd> BufferLineCycleNext <CR>", "Goto next buffer" },
 
-    ["<S-tab>"] = { "<cmd> BufferLineCyclePrev <CR>", "Goto prev buffer"},
+  ["<S-tab>"] = { "<cmd> BufferLineCyclePrev <CR>", "Goto prev buffer" },
 
-    -- Add descriptions for empty shortcuts
-    ["p"] = { "p", "Paste"},
-    ["P"] = { "P", "Paste"},
+  -- Add descriptions for empty shortcuts
+  ["p"] = { "p", "Paste" },
+  ["P"] = { "P", "Paste" },
 }
 
 local leader_mappings_opts = {
-      mode = "n", -- NORMAL mode
-      prefix = "<leader>",
-      buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-      silent = true, -- use `silent` when creating keymaps
-      noremap = true, -- use `noremap` when creating keymaps
-      nowait = true, -- use `nowait` when creating keymaps
-    }
+  mode = "n",         -- NORMAL mode
+  prefix = "<leader>",
+  buffer = nil,       -- Global mappings. Specify a buffer number for buffer local mappings
+  silent = true,      -- use `silent` when creating keymaps
+  noremap = true,     -- use `noremap` when creating keymaps
+  nowait = true,      -- use `nowait` when creating keymaps
+}
 
 local leader_mappings = {
-    -- Set line number
-    n = {
-      name = "LineNumbers",
-      ["r"] = { ":set relativenumber!<CR>", "Toggle relative number" },
-      ["n"] = { ":set number!<CR>", "Toggle number" },
-    },
+  -- Set line number
+  n = {
+    name = "LineNumbers",
+    ["r"] = { ":set relativenumber!<CR>", "Toggle relative number" },
+    ["n"] = { ":set number!<CR>", "Toggle number" },
+  },
+
+  s = {
+   ["F"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "Find in current [F]ile" }
+  }
 }
 
 local which_key = require "which-key"
 which_key.register(mappings, opts)
 which_key.register(leader_mappings, leader_mappings_opts)
-which_key.register({ 
+which_key.register({
   ["="] = {
     name = "+Yanky"
   }
 })
-which_key.register({ 
+which_key.register({
   ["["] = {
     name = "+Previous motion"
   }
 })
-which_key.register({ 
+which_key.register({
   ["]"] = {
     name = "+Next motion"
   }
 })
-which_key.register({ 
+which_key.register({
   ["\\"] = {
     name = "+VM-*"
   }
 })
-which_key.register({ 
+which_key.register({
   ["g"] = {
     name = "+Go to"
   }
 })
-which_key.register({ 
+which_key.register({
   ["z"] = {
     name = "+Fold"
   }
--- lua/custom/config/which-key.lua
+++ lua/custom/config/which-key.lua
@@ -1,28 +1,41 @@
 lvim.builtin.which_key.setup.plugins.marks = true
 lvim.builtin.which_key.setup.plugins.registers = true
-lvim.builtin.which_key.setup.plugins.presets.operators = true
+lvim.builtin.which_key.setup.plugins.presets.operators = false
 lvim.builtin.which_key.setup.plugins.presets.motions = true
 lvim.builtin.which_key.setup.plugins.presets.text_objects = true
 lvim.builtin.which_key.setup.plugins.presets.windows = true
 lvim.builtin.which_key.setup.plugins.presets.nav = true
 lvim.builtin.which_key.setup.plugins.presets.z = true
 lvim.builtin.which_key.setup.plugins.presets.g = true
-lvim.builtin.which_key.setup.window.padding = { 1, 2, 1, 2}
+lvim.builtin.which_key.setup.window.padding = { 1, 2, 1, 2 }
 lvim.builtin.which_key.setup.ignore_missing = false
+
+lvim.builtin.which_key.setup.operators = {
+  d = "Delete",
+  c = "Change",
+  y = "Yank (copy)",
+  ["g~"] = "Toggle case",
+  ["gu"] = "Lowercase",
+  ["gU"] = "Uppercase",
+  [">"] = "Indent right",
+  ["<lt>"] = "Indent left",
+  ["zf"] = "Create fold",
+  ["!"] = "Filter though external program",
+}
 
 vim.o.timeout = true
 vim.o.timeoutlen = 250
 
 
 -- lvim.builtin.which_key.on_config_done = function ()
-  -- local keymaps = vim.api.nvim_get_keymap("n")
-  -- local Log = require "lvim.core.log"
-  -- Log:debug "abcd"
-  -- print(vim.inspect(keymaps))
-  -- Log:debug(vim.inspect(keymaps))
-  -- for _, keymap in pairs(keymaps) do
-  --   if keymap ~= nil then
-  --     Log:debug(keymap.lhs)
-  --   end
-  -- end
+-- local keymaps = vim.api.nvim_get_keymap("n")
+-- local Log = require "lvim.core.log"
+-- Log:debug "abcd"
+-- print(vim.inspect(keymaps))
+-- Log:debug(vim.inspect(keymaps))
+-- for _, keymap in pairs(keymaps) do
+--   if keymap ~= nil then
+--     Log:debug(keymap.lhs)
+--   end
+-- end
 -- end
