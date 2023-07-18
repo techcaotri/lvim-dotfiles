690b2e1b0b1f24a3760d408de8d7e8e4e0f48143
Tri Pham <techcaotri@gmail.com>
2023-07-18 13:09:41 +0700

    Add 'folke/flash.nvim' plugin to flashy vim motion

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -331,4 +331,17 @@ lvim.plugins = {
       'nvim-tree/nvim-web-devicons'      -- optional
     }
   },
+
+  -- Motion plugins
+  {
+    'folke/flash.nvim',
+    opts = {},
+    keys = {
+      { "<leader>F", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "<leader>S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r",         mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R",         mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>",     mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+    },
+  }
 }
