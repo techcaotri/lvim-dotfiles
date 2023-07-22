626517f026c756522293ca9284dfe0ed08fb5cb0
Tri Pham <techcaotri@gmail.com>
2023-07-22 14:13:23 +0700

    Possesion group in workspace and sort by time

--- lua/custom/config/alpha.lua
+++ lua/custom/config/alpha.lua
@@ -1,20 +1,34 @@
 -- Dashboard configurations
 
-local lazy_entry = { "l", "  Lazy", ":Lazy<CR>"}
+local query = require('possession.query')
+
+local lazy_entry = { "l", "  Lazy", ":Lazy<CR>" }
 table.insert(lvim.builtin.alpha.dashboard.section.buttons.entries, lazy_entry)
 
 local new_entries =
     (function()
-      local group = {}
-      local path = vim.fn.stdpath("data") .. "/possession"
-      local files = vim.split(vim.fn.glob(path .. "/*.json"), "\n")
-      for i, file in pairs(files) do
-        local basename = vim.fs.basename(file):gsub("%.json", "")
-        local button = { tostring(i), "  " .. basename,
-          "<cmd>PossessionLoad " .. basename .. "<cr>" }
-        table.insert(group, button)
+      local sessions = query.as_list()
+      if (sessions == nil) then
+        local group = {}
+        local path = vim.fn.stdpath("data") .. "/possession"
+        local files = vim.split(vim.fn.glob(path .. "/*.json"), "\n")
+        for i, file in pairs(files) do
+          local basename = vim.fs.basename(file):gsub("%.json", "")
+          local button = { tostring(i), "  " .. basename,
+            "<cmd>PossessionLoad " .. basename .. "<cr>" }
+          table.insert(group, button)
+        end
+        return group
+      else
+        query.sort_by(sessions, "mtime", true)
+        local group = {}
+        for i, session in pairs(sessions) do
+          local button = { tostring(i), "  " .. session.name,
+            "<cmd>PossessionLoad " .. session.name .. "<cr>" }
+          table.insert(group, button)
+        end
+        return group
       end
-      return group
     end)()
 
 for _, entry in ipairs(new_entries) do
