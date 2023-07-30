7034824eacc9eaf8b42fccad7f9cb489f5d2f4e3
Tri Pham <techcaotri@gmail.com>
2023-07-30 13:53:39 +0700

    nvim-tree: Don't sync root with cwd

--- lua/custom/config/nvim-tree.lua
+++ lua/custom/config/nvim-tree.lua
@@ -1,5 +1,4 @@
 lvim.builtin.nvimtree.setup.update_focused_file.update_root = false
 -- lvim.builtin.nvimtree.setup.view.number = true
 lvim.builtin.nvimtree.setup.view.relativenumber = true
-
-
+lvim.builtin.nvimtree.setup.sync_root_with_cwd = false
