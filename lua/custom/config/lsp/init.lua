41e8d9df84b6904ea777d073fb946bd3fd833176
Tri Pham <techcaotri@gmail.com>
2023-08-03 17:25:44 +0700

    Fix inlay_hints not showing

--- lua/custom/config/lsp/init.lua
+++ lua/custom/config/lsp/init.lua
@@ -15,5 +15,21 @@ require('custom.config.autocmd').autocmd('LspAttach', {
 
     nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
     nmap('<leader>lh', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+    local clients = vim.lsp.get_active_clients({ bufnr = ev.buf })
+    if #clients > 0 then
+      for _, client in ipairs(clients) do
+        if client.server_capabilities.inlayHintProvider then
+          -- vim.notify(vim.inspect(client))
+          if client.name == 'clangd' then
+            -- Show inlay hints using clangd_extensions
+            require("clangd_extensions.inlay_hints").set_inlay_hints()
+          else
+            -- Show inlay_hints using the new 0.10 nvim's lsp feature
+            vim.lsp.inlay_hint(ev.buf, true)
+          end
+        end
+      end
+    end
   end,
 })
