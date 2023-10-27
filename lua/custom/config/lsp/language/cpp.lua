81ff6476f0bb5733d1ad74674511fadd36686d0d
Tri Pham <techcaotri@gmail.com>
2023-10-27 10:16:19 +0700

    Enable all default feature from ccls and clangd

--- lua/custom/config/lsp/language/cpp.lua
+++ lua/custom/config/lsp/language/cpp.lua
@@ -52,28 +52,8 @@ require("lvim.lsp.manager").setup("ccls", {
   on_attach = function(client)
     -- This list is copied from https://github.com/MaskRay/ccls/blob/ee2d4f5b9a2181e2c71341d34c7d2463f0c28cd1/src/messages/initialize.cc #L104
     -- textDocumentSync = true, -- Comment out since adding this will make 'ccls' stop working
-    client.server_capabilities.hoverProvider = false
-    client.server_capabilitiescompletionProvider = false
     -- signatureHelpProvider = false, -- Comment out since adding this will make 'ccls' stop working
-    client.server_capabilities.declarationProvider = false
-    client.server_capabilities.definitionProvider = false
-    client.server_capabilities.implementationProvider = true
-    client.server_capabilities.typeDefinitionProvider = false
-    client.server_capabilities.referencesProvider = false
-    client.server_capabilities.documentHighlightProvider = false
-    client.server_capabilities.documentSymbolProvider = false
-    client.server_capabilities.workspaceSymbolProvider = false
-    client.server_capabilities.codeActionProvider = false
-    client.server_capabilities.codeLensProvider = false
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentRangeFormattingProvider = false
-    client.server_capabilities.documentOnTypeFormattingProvider = false
-    client.server_capabilities.renameProvider = false
-    client.server_capabilities.documentLinkProvider = false
-    client.server_capabilities.foldingRangeProvider = false
-    client.server_capabilities.executeCommandProvider = false
     -- callHierarchyProvider = true -- Comment out since adding this will make 'ccls' stop working
-    client.server_capabilities.workspace = false
   end
 })
 
