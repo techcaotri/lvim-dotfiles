5e6bbdcf0fba36590daf32f7fb0cc763d332c352
Tri Pham <techcaotri@gmail.com>
2024-03-03 17:47:11 +0700

    Add '.python311-venv' to search names list of Python's venv-selector

--- lua/custom/config/venv-selector.lua
+++ lua/custom/config/venv-selector.lua
@@ -52,7 +52,7 @@ function M.config()
     parents = 2,
 
     -- name (default: venv) - The name of the venv directories to look for.
-    name = { "venv", ".venv", ".linux-venv", ".venv-python310" },     -- NOTE: You can also use a lua table here for multiple names: {"venv", ".venv"}`
+    name = { "venv", ".venv", ".linux-venv", ".venv-python310", ".python311-venv" },     -- NOTE: You can also use a lua table here for multiple names: {"venv", ".venv"}`
 
     -- fd_binary_name (default: fd) - The name of the fd binary on your system. Some Debian based Linux Distributions like Ubuntu use ´fdfind´.
     fd_binary_name = "fd",
