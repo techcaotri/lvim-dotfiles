1ae20af8fae28f97dc921f4b9be846e47ea206aa
Tri Pham <techcaotri@gmail.com>
2024-04-05 12:53:37 +0700

    Add 'flutter-tools.nvim' configs to support debugging with DAP, working with this repo 'git@github.com:techcaotri/gitfinder.git'

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -643,7 +643,39 @@ lvim.plugins = {
       'nvim-lua/plenary.nvim',
       'stevearc/dressing.nvim', -- optional for vim.ui.select
     },
-    config = true,
+    config = function()
+      require('flutter-tools').setup {
+       -- (uncomment below line for windows only)
+       -- flutter_path = "home/flutter/bin/flutter.bat",
+
+        debugger = {
+          -- make these two params true to enable debug mode
+          enabled = true,
+          run_via_dap = true,
+          register_configurations = function(_)
+
+            require("dap").configurations.dart = {
+              {
+                type = "dart",
+                request = "launch",
+                name = "Launch flutter",
+                dartSdkPath = '/home/tripham/Dev/flutter/bin/cache/dart-sdk/',
+                flutterSdkPath = "/home/tripham/Dev/flutter",
+                program = "${workspaceFolder}/lib/main.dart",
+                cwd = "${workspaceFolder}",
+              }
+            }
+            -- uncomment below line if you've launch.json file already in your vscode setup
+            require("dap.ext.vscode").load_launchjs()
+          end,
+        },
+        dev_log = {
+          -- toggle it when you run without DAP
+          enabled = false,
+          open_cmd = "tabedit",
+        },
+      }
+    end,
   },
 
   {
