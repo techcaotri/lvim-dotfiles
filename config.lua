a167288bde7523f086245661abd5d2ddac584666
Tri Pham <techcaotri@gmail.com>
2024-08-28 15:06:18 +0700

    Add 'Redir' for copying output of lua command to clipboard

--- config.lua
+++ config.lua
@@ -125,6 +125,15 @@ vim.g.clipboard = {
   cache_enabled = true
 }
 
+-- Add 'Redir' for copy output of lua command to clipboard
+-- Usage: :Redir lua=vim.tbl_keys(package.loaded)
+vim.api.nvim_create_user_command('Redir', function(ctx)
+  local lines = vim.split(vim.api.nvim_exec(ctx.args, true), '\n', { plain = true })
+  vim.cmd('new')
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+  vim.opt_local.modified = false
+end, { nargs = '+', complete = 'command' })
+
 -- Auto reload file when it's changed on disk
 local autoread = vim.api.nvim_create_augroup('ar_autoread', { clear = true })
 vim.api.nvim_create_autocmd('FileChangedShellPost', {
