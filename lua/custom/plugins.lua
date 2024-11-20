993c5e5302867fd488ef6c054ea82ee7bfbbff40
Tri Pham <techcaotri@gmail.com>
2024-11-20 15:50:35 +0700

    Add 'Hypersonic.nvim' for previewing RegEx

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -982,5 +982,17 @@ lvim.plugins = {
       "cbochs/grapple.nvim",
       "ThePrimeagen/harpoon"
     },
-  }
+  },
+
+  -- regex
+  {
+    'tomiis4/Hypersonic.nvim',
+    event = "CmdlineEnter",
+    cmd = "Hypersonic",
+    config = function()
+      require('hypersonic').setup({
+        -- config
+      })
+    end
+  },
 }
