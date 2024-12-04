bf768a6a7be1f90b85823854e8e6cdfafc331824
Tri Pham <techcaotri@gmail.com>
2024-12-04 10:54:52 +0700

    Add cursor effect via "sphamba/smear-cursor.nvim"

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -995,4 +995,14 @@ lvim.plugins = {
       })
     end
   },
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      stiffness = 0.8,             -- 0.6      [0, 1]
+      trailing_stiffness = 0.5,    -- 0.25     [0, 1]
+      distance_stop_animating = 0.5, -- 0.1      > 0
+      hide_target_hack = false,    -- true     boolean
+      legacy_computing_symbols_support = true,
+    },
+  },
 }
