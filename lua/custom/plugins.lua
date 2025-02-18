902f2c66874b0f055fafac9b9da41cc55048b1fd
Tri Pham <techcaotri@gmail.com>
2025-02-18 14:55:14 +0700

    Remove 'image.nvim' for better performance when editing Markdown file

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -166,6 +166,9 @@ lvim.plugins = {
       "nvim-lua/plenary.nvim",
     },
   },
+  {
+    "tpope/vim-fugitive",
+  },
   { "folke/neodev.nvim", opts = {} },
   {
     "mbbill/undotree",
@@ -791,55 +794,6 @@ lvim.plugins = {
     priority = 1001,
     opts = {
       rocks = { "magick" },
-    },
-  },
-  {
-    "3rd/image.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-          require("nvim-treesitter.configs").setup({
-            highlight = {
-              enable = true,
-            },
-            ensure_installed = {
-              "vimdoc",
-              "luadoc",
-              "vim",
-              "lua",
-              "markdown"
-            },
-          })
-        end,
-      },
-    },
-    opts = {
-      backend = "kitty",
-      integrations = {
-        markdown = {
-          enabled = true,
-          clear_in_insert_mode = false,
-          download_remote_images = true,
-          only_render_image_at_cursor = false,
-          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
-        },
-        neorg = {
-          enabled = true,
-          clear_in_insert_mode = false,
-          download_remote_images = true,
-          only_render_image_at_cursor = false,
-          filetypes = { "norg" },
-        },
-      },
-      max_width = nil,
-      max_height = nil,
-      max_width_window_percentage = nil,
-      max_height_window_percentage = 50,
-      tmux_show_only_in_active_window = true,
-      kitty_method = "normal",
     },
   },
 
