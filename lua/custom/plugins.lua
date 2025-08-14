e9e3c5281284b09e25989def3a7f078b973a6929
Tri Pham <techcaotri@gmail.com>
2025-08-14 16:50:53 +0700

    Add 'hedyhli/outline.nvim' plugin for outline sidebar

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1247,4 +1247,16 @@ lvim.plugins = {
       "AnsiEsc",
     },
   },
+  {
+    "hedyhli/outline.nvim",
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+        { desc = "Toggle Outline" })
+
+      require("outline").setup {
+        -- Your setup opts here (leave empty to use defaults)
+      }
+    end,
+  },
 }
