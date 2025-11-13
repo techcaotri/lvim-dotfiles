4c8fac6a86bf1981066c9839b2c27ff61c6c67df
Tri Pham <v.tripc2@vinai.io>
2025-11-13 14:45:43 +0700

    Update Node standalone scripts run and debug

--- config.lua
+++ config.lua
@@ -181,20 +181,25 @@ if lvim.builtin.nvimtree then
   lvim.builtin.nvimtree.setup.renderer = lvim.builtin.nvimtree.setup.renderer or {}
   lvim.builtin.nvimtree.setup.actions = lvim.builtin.nvimtree.setup.actions or {}
   lvim.builtin.nvimtree.setup.actions.file_popup = lvim.builtin.nvimtree.setup.actions.file_popup or {}
-  
+
   -- 1) Show long names in a floating window
   lvim.builtin.nvimtree.setup.renderer.full_name = true
-  
+
   -- 2) Tweak the floating window
   lvim.builtin.nvimtree.setup.actions.file_popup.open_win_config = vim.tbl_deep_extend(
     "force",
     lvim.builtin.nvimtree.setup.actions.file_popup.open_win_config or {},
     {
       relative = "cursor",
-      border = "rounded", 
+      border = "rounded",
       style = "minimal",
       row = 1,
       col = 1,
     }
   )
 end
+
+vim.api.nvim_create_user_command('RunNode', function(opts)
+  local file = vim.fn.expand('%:p') -- Get full path of current file
+  vim.cmd('vsplit term://node ' .. file)
+end, { desc = 'Run current Node.js file in a new vertical split terminal' })
